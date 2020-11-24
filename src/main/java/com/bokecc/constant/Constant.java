b3c05fc0382af61d087fc344bafc7ef6cd6f76e5
tmackan <kan49733110@163.com>
2020-11-24 17:45:31 +0800

    auth fix

--- src/main/java/com/bokecc/constant/Constant.java
+++ src/main/java/com/bokecc/constant/Constant.java
@@ -7,7 +7,7 @@ public class Constant {
     public static final int ERROR_HTTP_CODE = 200;
 
     // swagger文档 Basic Auth
-    public static final String BASIC_AUTH_USER = "common-user";
+    public static final String BASIC_AUTH_USER = "test";
     public static final String BASIC_AUTH_PW_ONLINE = "123";
     public static final String BASIC_AUTH_PW_TESTING = "123";
 
